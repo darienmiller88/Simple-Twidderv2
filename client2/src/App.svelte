d501f3b75d9d9194758cdcfde6d3263ea4874a99
darienmiller88 <darienmiller88@yahoo.com>
2023-08-19 03:51:16 -0400

    finished front end

--- client2/src/App.svelte
+++ client2/src/App.svelte
@@ -1,130 +1,78 @@
 <script lang="ts">
   import {type Tweed} from "./lib/types"
-  let name:  string = ""
+  import { Moon } from 'svelte-loading-spinners';
+  import "./tweed.scss"
+
+  let isLoading:    boolean = false
+  let name:         string = ""
   let tweedContent: string = ""
-  let tweeds: Tweed[] = []
+  let tweeds:       Tweed[] = [
+    {
+      name: "Darien",
+      content: "First tweed",
+      date: new Date()
+    },
+    {
+      name: "Dalton",
+      content: "dreads",
+      date: new Date()
+    },
+    {
+      name: "Vicky",
+      content: "spam!",
+      date: new Date()
+    }
+  ]
 
   function sendTweed(){
-    let tweed = {
-      name,
-      tweedContent
+    let tweed: Tweed = {
+      name: name,
+      content: tweedContent,
+      date: new Date()
     }
 
-    console.log(tweed)
+    tweeds = [...tweeds, tweed]
     name = ""
     tweedContent = ""
+    isLoading = !isLoading
   }
 </script>
 
 <main>
   <div class="title twidder-color">Twidder</div>
-  <form on:submit|preventDefault={sendTweed}>
-    <div class="name-container form-item">
-      <label for="name" class="twidder-color">Name</label><br />
-      <input placeholder="Enter name" bind:value={name} />
+
+  {#if !isLoading}
+    <form on:submit|preventDefault={sendTweed}>
+      <div class="name-container form-item">
+        <label for="name" class="twidder-color">Name</label><br />
+        <input placeholder="Enter name" bind:value={name} />
+      </div>
+      <br />
+      <div class="text-container form-item">
+        <label for="tweed" class="twidder-color">Tweed</label><br />
+        <textarea placeholder="Enter tweed" rows="5" bind:value={tweedContent} />
+      </div>
+      <button>Send Tweed!</button>
+    </form> 
+  {/if}
+
+  {#if isLoading}
+    <div class="loading">
+      <Moon size="200" color="#1DA1F2" unit="px" duration="0.75s" />
     </div>
-    <br />
-    <div class="text-container form-item">
-      <label for="tweed" class="twidder-color">Tweed</label><br />
-      <textarea placeholder="Enter tweed" rows="5" bind:value={tweedContent} />
-    </div>
-    <button>Send Tweed!</button>
-  </form>
+  {/if}
 
   <div class="tweeds">
-
+    {#each tweeds as tweed}
+      <div class="tweed">
+        <div class="name" id={tweed.date.toString() + tweed.name.at(0)}>{tweed.name}</div>
+        <div class="content" id={tweed.date.toString() + tweed.name.at(0)}>{tweed.content}</div>
+        <div class="date" id={tweed.date.toString() + tweed.name.at(0)}>{tweed.date.toString()}</div>
+      </div>
+    {/each}
   </div>
 </main>
 
 <style lang="scss">
-  .title{
-    text-align: center;
-    font-size: 70px;
-    margin: 10px 0px;
-    color: var(--twidder-blue);
-  }
-
-  form{
-    text-align: center;
-    width: 90%;
-    margin: auto;
-
-    .text-container{
-      padding: 10px 0px;
-            
-      label{
-        font-size: 20px;
-        color: var(--twidder-blue);
-      }
-
-      textarea{
-        width: 100%;
-        font-size: 20px;
-      }
-    }
-
-    .name-container{
-      label{
-        font-size: 20px;
-        color: var(--twidder-blue);
-      }
   
-      input{
-        width: 100%;
-        font-size: 20px;
-      }
-    }
-
-    button{
-      width: 80%;
-      padding: 15px 0px;
-      font-size: 20px;
-
-      border: 2px white solid;
-      border-radius: 10px;
-
-      color: white;
-      background-color: var(--twidder-blue);
-      transition: 0.5s;
-
-      &:hover{
-        cursor: pointer;
-        background-color: var(--twidder-blue-transparent);
-      }
-
-      &:active{
-        transform: translateY(-5px);
-      }
-    }
-
-    @media only screen and (min-width: 768px) {
-      width: 80%;
-    }
-
-    @media only screen and (min-width: 1200px) {
-      width: 50%;
-    }
-  }
-
-  .tweeds{
-    border: 2px solid black;
-    margin: auto;
-    width: 90%;
-    margin-top: 30px;
-
-    @media only screen and (min-width: 768px) {
-      width: 80%;
-    }
-
-    @media only screen and (min-width: 1200px) {
-      width: 50%;
-    }
-  }
-
-  .form-item{
-    text-align: left;
-    width: 80%;
-    margin: auto;
-    // text-align: left;
-  }
 </style>
--- client2/src/lib/types.ts
+++ client2/src/lib/types.ts
@@ -1,15 +1,5 @@
-export interface Person{
-    name: string
-    age: number
-}
-
-export interface Car{
-    make: string
-    type: string
-    year: number
-}
-
 export interface Tweed{
     name: string
     content: string
+    date: Date
 }
